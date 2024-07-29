aea43766c82cb7347cf02ec9c18e42ad2a21d411
Basava_Raja.k <kuruvabasavaraj09@gmail.com>
2024-07-29 19:31:10 +0530

    added sample src

---
+++ src/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.113.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "labresgrp1"
+    storage_account_name = "stgaccstate101"
+    container_name       = "statefilecotainer"
+    key                  = "devfile.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+}
