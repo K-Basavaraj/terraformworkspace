c93ef79a5ef2cc8d92eb5a9df41b5b5e2d6ca38d
Basava_Raja.k <kuruvabasavaraj09@gmail.com>
2024-08-17 22:40:46 +0530

    updated vnet module with subnet ids attributed and tested

--- terraform_module/virtual_network/outputs.tf
+++ terraform_module/virtual_network/outputs.tf
@@ -7,3 +7,18 @@ output "id" {
   value       = azurerm_virtual_network.vnet.id
   description = "id of the vnet"
 }
+
+output "resource_group_name" {
+  value       = azurerm_virtual_network.vnet.resource_group_name
+  description = "name of the resource group"
+}
+
+output "location" {
+  value       = azurerm_virtual_network.vnet.location
+  description = "location where vnet is created"
+}
+
+output "subnet_ids" {
+  value       = azurerm_virtual_network.vnet.subnet[*].id
+  description = "id of this"
+}
