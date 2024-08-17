313a9876c61feaa7d4c19be82480d09d8a413421
Basava_Raja.k <77600869+K-Basavaraj@users.noreply.github.com>
2024-08-17 22:46:04 +0530

    Feature branch 7 add subnet module (#9)
    * added subnet module
    
    * Revert "added subnet module"
    
    This reverts commit 63b456d1893785e060649e3f0eaf9b8336afae55.
    
    * updated vnet module with subnet ids attributed and tested

--- terraform_module/virtual_network/main.tf
+++ terraform_module/virtual_network/main.tf
@@ -12,8 +12,22 @@ resource "azurerm_virtual_network" "vnet" {
       }
     ]
     content {
-      id     = ddos_protection_plan.valaue.id
-      enable = ddos_protection_plan.valaue.enable
+      id     = ddos_protection_plan.value.id
+      enable = ddos_protection_plan.value.enable
+    }
+  }
+  dynamic "encryption" {
+    for_each = var.encryption
+    content {
+      enforcement = encryption.value.enforcement
+    }
+  }
+  dynamic "subnet" {
+    for_each = var.subnet != null ? var.subnet : []
+    content {
+      name           = subnet.value.name
+      address_prefix = subnet.value.address_prefix
+      security_group = subnet.value.security_group
     }
   }
   tags = var.tags
